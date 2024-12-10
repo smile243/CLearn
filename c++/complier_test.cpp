b8c3e9f65ef1e5c2a9c1565abdd4b9c349cc28ba
yjl <827359508@qq.com>
2024-12-10 22:26:59 +0800

    c++

---
+++ c++/complier_test.cpp
@@ -0,0 +1,5 @@
+//预处理阶段，替换代码
+#define INTERGER int
+INTERGER multiply(INTERGER a,INTERGER b){
+    return a*b;
+}
---
+++ c++/log.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+//定义
+void Log(const char* message){
+    std::cout<<message<<std::endl;
+}
