52cdd248a49ff0d079132fce05a197079d17fadc
yjl <827359508@qq.com>
2025-02-26 23:13:59 +0800

    vector

--- c++/std/vector.cpp
+++ c++/std/vector.cpp
@@ -9,25 +9,37 @@ private:
     int _age;
 public:
     Person(const std::string &name,int age):_name(name),_age(age)
-{
-    std::cout<<"constructor"<<std::endl;
-}
+    {
+        std::cout<<"constructor"<<std::endl;
+    }
+    Person(const Person & p):_name(p._name),_age(p._age){
+        std::cout<<"copied"<<std::endl;
+    }
 
-~Person()
-{
-     std::cout<<"del"<<std::endl;
-}
+    ~Person()
+    {
+        std::cout<<"del"<<std::endl;
+    }
 };
 
 /**
  * 初始化时要指定容量，不然会有多余自动扩容带来的复制操作
  * push_back会调用一次构造函数+一次移动构造函数；emplace_back只调用一次构造函数
+ * vector扩容因子是1.5
 */
 int main()
 {
     std::vector<Person> vector;
     vector.reserve(3);
-    vector.emplace_back(Person("zs",12));
-    vector.emplace_back(Person("ls",13));
-    vector.emplace_back(Person("ww",14));
+    //push 是函数内部构造然后复制到vector中
+    // vector.push_back(Person("zs",12));
+    // vector.push_back(Person("ls",13));
+    // vector.push_back(Person("ww",14));
+    //emplace_back直接在vector中构造，不存在复制
+    vector.emplace_back("zs",12);
+    vector.emplace_back("ls",13);
+    vector.emplace_back("ww",14);
+    // for(const Person& p:vector){
+    //     std::cout<<"123"<<std::endl;
+    // }
 }
